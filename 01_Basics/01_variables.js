36ac60f35b32c96d17a1388b31a9579add85f348
Harshraj-Gupta <139990406+Harshraj-Gupta@users.noreply.github.com>
2023-09-03 16:01:03 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 365465
+let accountEmail = "sanju78@gmail.com"
+var acoountPassword = "65765"
+accountCity = "UP"
+let accountState; //undefined variable
+ 
+// accountId=67 not allowed
+console.log(accountId);
+
+/*
+    Prefer not use to var
+    because of issue in block scope and functional scope
+ */
+
+accountEmail = "khgkj@hjlj"
+accountPasssword = "089098"
+accountCity = "Gujrat"
+console.table([accountEmail, accountId, accountPasssword, accountCity, accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+ // following are the datatypes
+
+// alert(3 + 3) // we are using node js, not browser
+
+// boolean => true/false
+// String = ""
+// number = => 2 to the power 53
+// bigInt 
+// null = standalone value and is an object
+// symbol => unique
+// undefined 
+// object
+
+
+let age = 18
+let name = "Harsh"
+let isLoggedIn = false
+let state = undefined
+let name1 = null // object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof false)
+console.log(typeof null) // object and cosidered as flaw of this language
+console.log(typeof undefined)
+
+//  To Know Standard of JS visit tc39.cs website
+
+
+
