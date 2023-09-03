42aa8703147b76eac44f9bb9b2a6f53205095619
Harshraj-Gupta <139990406+Harshraj-Gupta@users.noreply.github.com>
2023-09-03 17:14:24 +0000

    Conversion of datatypes

---
+++ 01_Basics/ConversionOperation.js
@@ -0,0 +1,37 @@
+//let score = "100"
+//let score = "56abc"
+//let score = null
+//let score = undefined
+// let score = "Sanju"
+let score = true
+
+ // console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "100" => 100
+// "56abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+let isLoggedIn = "Sanju"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Sanju" => true
+
+let someNumber = 56
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
