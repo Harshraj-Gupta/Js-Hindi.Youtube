a904f4d70cab08a7c8cd6707ad4b96da767f3be3
Harshraj-Gupta <139990406+Harshraj-Gupta@users.noreply.github.com>
2023-09-04 05:59:12 +0000

    Comparision

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,22 @@
+//console.log(2 < 1);
+//console.log(2 <= 1);
+//console.log(2 > 1);
+//console.log(2 == 3);
+//console.log(2 != 1);
+
+console.log("2" > 1); //true
+console.log("02" > 1); //true
+
+console.log(null > 0); //false
+console.log(null == 0) //false
+console.log(null >= 0); //true
+
+console.log(undefined > 0); //false
+console.log(undefined < 0); // false
+console.log(undefined == 0); // false
+console.log("5" === 9); //false
+// ===  => it checks both equality and datatype
+
+// ************** Summary **************
+
+// avoid to use all the above expressions
