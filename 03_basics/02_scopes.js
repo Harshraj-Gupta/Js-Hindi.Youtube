2f9ac3a026d030994fb5c25ea7d2d0f44d4f94a7
Harshraj-Gupta <139990406+Harshraj-Gupta@users.noreply.github.com>
2023-09-14 18:27:40 +0000

    Scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+// let a = 10
+// const b = 20
+var c = 30
+let a = 5765
+
+if(true){
+    let a = 20;
+    const b = 87;
+    var c = 373;
+    console.log("Inner: ",a);
+    console.log(b);
+
+}
+console.log(a);
+// console.log(b);
+console.log(c);
