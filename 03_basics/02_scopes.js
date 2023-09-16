512f27dc3f5bcac8a69c88ce178ee5fe43d182e0
Harshraj-Gupta <139990406+Harshraj-Gupta@users.noreply.github.com>
2023-09-16 14:58:37 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,10 +7,54 @@ if(true){
     let a = 20;
     const b = 87;
     var c = 373;
-    console.log("Inner: ",a);
-    console.log(b);
+    // console.log("Inner: ",a);
+    // console.log(b);
 
 }
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+function one()
+{
+    const  username = "Harsh"
+    function two()
+    {
+        const website = "Youtube "
+        // console.log(username);
+    }
+    // console.log(website);   
+
+    two()
+}
+
+// one() 
+
+if (true){
+    const username = "Harshraj"
+    if(username == "Harshraj")
+    {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+
+}
+// console.log(username);
+
+
+// +++++++++++++++++++ Interesting Topic ++++++++++++++++++++++
+
+//  console.log(addOne(8)). ///. No Error
+  function addOne(num)
+  {
+    return num + 1
+  }
+
+//   addTwo(5).  // Error
+  const addTwo = function(num)
+  {
+      return num + 2
+    }
+    console.log(addTwo(5))
+    
