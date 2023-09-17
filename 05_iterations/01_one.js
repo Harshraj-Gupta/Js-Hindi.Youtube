41c5b631190f0315b8921e1fbf27ad9acac31021
Harshraj-Gupta <139990406+Harshraj-Gupta@users.noreply.github.com>
2023-09-17 07:07:41 +0000

    For loop

--- 05_iterations/01_one.js
+++ 05_iterations/01_one.js
@@ -1,6 +1,35 @@
 // For loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let i = 0; i <=10; i++) {
+    const element = i;
+    // console.log(element);
     
+}
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    // console.log(`Outer Loop: ${i}` );
+    for (let j = 1; j <= 10; j++) {
+        const element = j;
+    // console.log(`Inner Loop Inner Loop: ${j}` );  
+    // console.log(i + '*' + j + ' = ' + i*j);
+    }
+    
+}
+
+// +++++++++++++++++++ break and continue +++++++++++++++++
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//     console.log( "Detected 5");
+//       break
+//      }
+//  console.log(`value of i is ${index}`);
+// }
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+    console.log( "Detected 5");
+      continue
+     }
+ console.log(`value of i is ${index}`); 
 }
